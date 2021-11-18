9d2a422d2c59723951ef0544e8fd28ec84888489
Щербаков Руслан Владиславович (4040270) <scherbakovr@dev.vtb.ru>
2021-11-18 15:16:50 +0300

    Asmx service

---
+++ AsmxWebApplication/AsmxApplicationService/AsmxApplicationService/Individual.cs
@@ -0,0 +1,27 @@
+﻿namespace AsmxApplicationService
+{
+    [System.Serializable]
+    public class Individual
+    {
+        private int _operationResult;
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        [System.Xml.Serialization.XmlIgnore]
+        public int SomeImportantAndPrivateOperation
+        {
+            get { return _operationResult; }
+            set { _operationResult = value; }
+        }
+
+        public Individual() { }
+
+        public Individual(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+    }
+}

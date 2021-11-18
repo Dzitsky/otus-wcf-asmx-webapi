88bf0fea079c81df5a8d8615a54833c73fe0ca1c
Щербаков Руслан Владиславович (4040270) <scherbakovr@dev.vtb.ru>
2021-11-18 17:09:53 +0300

    Add console WCF consumer

---
+++ WcfWebApplication/WcfConsoleApplicationConsumer/WcfConsoleApplicationConsumer/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using WcfConsoleApplicationConsumer.ServiceReference1;
+
+namespace WcfConsoleApplicationConsumer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 
+            Service1Client service1Client = new Service1Client();
+
+            var compositeType = new CompositeType()
+            {
+                BoolValue = true,
+                StringValue = "This string will have been added by "
+            };
+
+            var result = service1Client.GetDataUsingDataContract(compositeType); // GetDataUsingDataContractAsync
+
+            Console.WriteLine(result.StringValue);
+
+            var nextResult = service1Client.GetData(100); // GetDataAsync
+
+            Console.WriteLine(nextResult);
+
+            Console.ReadKey();
+        }
+    }
+}
